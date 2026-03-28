4b53dc80aa945850f48011add403e077ad797f65
appsyouneed <humanityliberator@gmail.com>
2026-03-28 18:57:09 -0400

    Add files via upload

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-ssh-keygen -f '/root/.ssh/known_hosts' -R '194.93.48.12' 2>/dev/null || true
+ssh-keygen -f "/root/.ssh/known_hosts" -R "194.93.48.12" 2>/dev/null || true
 ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 root@194.93.48.12
-echo ""
-echo "To connect manually, run:"
-echo "ssh root@194.93.48.12"
